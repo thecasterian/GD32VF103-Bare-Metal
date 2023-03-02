1a015b2cf5575f5b0f98c0e9efe9c3615e7f4159
Jeongu Kim <jeongukim@kaist.ac.kr>
2023-03-03 06:04:18 +0900

    Update gitignore and Makefile

--- common/definition.mk
+++ common/definition.mk
@@ -1,13 +1,25 @@
+INCLUDE_DIR = inc
+SOURCE_DIR = src
+BUILD_DIR = build
+
+# Toolchain definitions.
+TOOLCHAIN = riscv32-unknown-elf
+CC = $(TOOLCHAIN)-gcc
+OC = $(TOOLCHAIN)-objcopy
+OD = $(TOOLCHAIN)-objdump
+SZ = $(TOOLCHAIN)-size
+DB = $(TOOLCHAIN)-gdb
+
 OPT = -O0
 WRN = -Wall -Wextra
 ARCH = -march=rv32imac -mabi=ilp32 -mcmodel=medlow
 
-# Assembler flags
+# Assembler flags.
 ASFLAGS  = $(OPT)
 ASFLAGS += $(WRN)
 ASFLAGS += $(ARCH)
 
-# Compiler flags
+# Compiler flags.
 CFLAGS  = $(OPT)
 CFLAGS += $(WRN)
 CFLAGS += -g3
@@ -16,7 +28,7 @@ CFLAGS += --specs=nano.specs
 CFLAGS += --specs=nosys.specs
 CFLAGS += $(ARCH)
 
-# Linker flags
+# Linker flags.
 LDFLAGS  = $(OPT)
 LDFLAGS += $(WRN)
 LDFLAGS += $(ARCH)
@@ -24,4 +36,3 @@ LDFLAGS += -nostartfiles
 LDFLAGS += -nostdlib
 LDFLAGS += -Wl,--no-relax
 LDFLAGS += -Wl,--gc-sections
-LDFLAGS += -T$(DEVICE).ld
--- common/toolchain.mk
+++
@@ -1,7 +0,0 @@
-# Toolchain definitions.
-TOOLCHAIN = riscv32-unknown-elf
-CC = $(TOOLCHAIN)-gcc
-OC = $(TOOLCHAIN)-objcopy
-OD = $(TOOLCHAIN)-objdump
-SZ = $(TOOLCHAIN)-size
-DB = $(TOOLCHAIN)-gdb
